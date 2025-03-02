8c6eadb304e711cb14f3591bc4b71bb22b447693
DiegoBeja <diego.bejarano@uabc.edu.mx>
2025-03-01 22:04:44 -0800

    Act 4

--- src/main/java/com/mycompany/calculadoracomplejos/NumeroComplejo.java
+++ src/main/java/com/mycompany/calculadoracomplejos/NumeroComplejo.java
@@ -1,31 +1,44 @@
 package com.mycompany.calculadoracomplejos;
 
 public class NumeroComplejo {
-    private int parteReal;
-    private int parteImaginaria;
+    private float parteReal;
+    private float parteImaginaria;
     
     public NumeroComplejo(){
         
     }
     
-    public NumeroComplejo(int parteReal, int parteImaginaria){
+    public NumeroComplejo(float parteReal, float parteImaginaria){
         this.parteReal = parteReal;
         this.parteImaginaria = parteImaginaria;  
     }
     
-    public void setReal(int real){
+    public void setReal(float real){
         this.parteReal = real;
     }
     
-    public int getReal(){
+    public float getReal(){
         return parteReal;
     }
     
-    public void setImaginario(int imaginario){
+    public void setImaginario(float imaginario){
         this.parteImaginaria = imaginario;
     }
     
-    public int getImaginario(){
+    public float getImaginario(){
         return this.parteImaginaria;
+    }
+    
+    @Override
+    public String toString(){
+        if(parteImaginaria == 0){
+            return parteReal + "";
+        } else if(parteReal == 0){
+            return parteImaginaria + "i";
+        } else if(parteImaginaria < 0){
+            return parteReal + "-" + (-parteImaginaria) + "i";
+        } else {
+            return parteReal + "+" + parteImaginaria + "i";
+        }
     }
 }
